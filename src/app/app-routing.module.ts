f491ff21cd8145791ecb4d205168c509ae3c0199
abdulaffou <gangawaliaffou@gmail.com>
2020-01-22 13:15:56 +0530

    login router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { ClassComponent } from './class/class.component';
 import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
 {path : 'studentlist',component: ListComponent},
-{ path:'profile',component: ProfileComponent }
+{ path:'profile',component: ProfileComponent },
+{ path:'login', component: LoginComponent}
 ];
 
 
@@ -19,5 +21,6 @@ const routes: Routes = [
 export class AppRoutingModule { }
  export const routingcomponents=[
    ListComponent,
-   ProfileComponent
+   ProfileComponent,
+   LoginComponent
  ];
