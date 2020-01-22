6c8872cd5225aac9ef2840f8e5bb4108da7c9e1f
YajnaJ <yajnaj1999@gmail.com>
2020-01-22 11:14:09 +0530

    sign up screen design

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  imageUrl:string="/assets/signup/img.png"; 
+fileToUpload: File=null;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  handleFileInput(file:FileList){
+    this.fileToUpload=file.item(0);
+    var reader=new FileReader();
+    reader.onload=(event:any) => {
+      this.imageUrl =event.target.result;
+    }
+  }
+
+}
