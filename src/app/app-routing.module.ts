25aa4b6d3036f540a4eff9f274b6c4694d53eeb3
abdulaffou <gangawaliaffou@gmail.com>
2020-01-23 10:40:09 +0530

    modifed login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { ProfileComponent } from './profile/profile.component';
 import { ListComponent } from './list/list.component';
 import { ClasslistComponent } from './classlist/classlist.component';
 import { LoginComponent } from './login/login.component';
+import { OverlayComponent } from './overlay/overlay.component';
 
 
 const routes: Routes = [
   { path:'register' , component:RegisterComponent },
-  {path : 'studentlist',component: ListComponent},
-{ path:'profile',component: ProfileComponent },
-{path: 'classlist',component:ClasslistComponent},
-{ path:'login', component: LoginComponent}
+  { path : 'studentlist',component: ListComponent},
+  { path:'profile',component: ProfileComponent },
+  { path: 'classlist',component:ClasslistComponent},
+  { path:'login', component: LoginComponent},
+  { path:'overlay', component: OverlayComponent}
 ];
 
 
@@ -29,4 +31,5 @@ export class AppRoutingModule { }
    RegisterComponent,
    ClasslistComponent,
    LoginComponent,
+   OverlayComponent
  ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingcomponents} from './app-routing.module';
 import { FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule}  from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { ClassComponent } from './class/class.component';
-import { ProfileComponent } from './profile/profile.component';
-
-
 import { AppComponent } from './app.component';
-import { RegisterComponent} from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ClassComponent,
-   routingcomponents,
-   ProfileComponent,
-   LoginComponent,
-    RegisterComponent,
+   routingcomponents
   ],
   imports: [
     BrowserModule,
