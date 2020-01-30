906ace965a4cff5ff4b96e50c8d7fcc55820d050
kushal-p1699 <kushal.p1699@gmail.com>
2020-01-29 19:51:39 -0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { ProfileComponent } from './profile/profile.component';
@@ -18,15 +17,6 @@ import { UploadProfileComponent } from './upload-profile/upload-profile.componen
 import { DateselectorComponent } from './dateselector/dateselector.component';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,30 +31,15 @@ import { HttpClientModule } from '@angular/common/http';
     DateselectorComponent,
     ConfirmDialogComponent,
     ForgotPasswordComponent,
-    MainNavComponent,
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     MatDialogModule,
     MatDatepickerModule,
-    MatNativeDateModule,
-<<<<<<< HEAD
-    HttpClientModule
-=======
-    HttpClientModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    HttpClientModule
-
->>>>>>> 7cd6edc786c8b4c8b6cd0432373232dd19522796
+    MatNativeDateModule
   ],
   entryComponents: [
     UploadProfileComponent,
