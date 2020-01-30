9e1743c493e83e0da4aa06b07e18096a7091e45f
kushal-p1699 <kushal.p1699@gmail.com>
2020-01-29 20:08:13 -0800

    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,6 @@ import { UploadProfileComponent } from './upload-profile/upload-profile.componen
 import { DateselectorComponent } from './dateselector/dateselector.component';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-<<<<<<< HEAD
-=======
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -28,7 +26,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {HttpClientModule} from  '@angular/common/http';
 import { from } from 'rxjs';
->>>>>>> d3eb8c8d0a4fe842935b7e399fb5e389746906aa
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,9 +48,6 @@ import { from } from 'rxjs';
     FormsModule,
     MatDialogModule,
     MatDatepickerModule,
-<<<<<<< HEAD
-    MatNativeDateModule
-=======
     MatNativeDateModule,
     // HttpClientModule,
     LayoutModule,
@@ -64,7 +58,6 @@ import { from } from 'rxjs';
     MatListModule,
     HttpClientModule
 
->>>>>>> d3eb8c8d0a4fe842935b7e399fb5e389746906aa
   ],
   entryComponents: [
     UploadProfileComponent,
