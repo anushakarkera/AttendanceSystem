3971cf0d0e64807ac9f5daa13e333cc6dac808d9
kushal-p1699 <kushal.p1699@gmail.com>
2020-02-03 08:41:00 -0800

    h

--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -9,7 +9,7 @@ export class ProfileService {
   constructor(private httpclient : HttpClient) { }
 
   getProfileDetails() : Promise<any>{
-    return this.httpclient.post("http://juegostudio.in:3021/user/login","testing")
+    return this.httpclient.post("	http://dummy.restapiexample.com/api/v1/create"," ")
     .toPromise();
   }
 }
