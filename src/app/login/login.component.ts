581a96a55b08460494469baf8f6ab62b8ace92e3
kushal-p1699 <kushal.p1699@gmail.com>
2020-02-02 19:47:59 -0800

    login api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { LoginService } from '../service1/login.service';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,21 @@ export class LoginComponent implements OnInit {
 
   public username = "";
   public password = "";
-  constructor(private router: Router) { }
+  constructor(private router: Router , private _loginService : LoginService) { }
 
   ngOnInit() {
+
   }
 
 
   onClick(){
     console.log(this.username);
     console.log(this.password);
+
+    this._loginService.getLoginDetails(this.username,this.password).subscribe((data) => {
+      console.log(data);
+    })
+
   }
   onSignup(){
     this.router.navigate(['/register']);
