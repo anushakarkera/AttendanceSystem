11a1f93420d963fff1eb8ff1bd2858aa5c550d57
abdulaffou <gangawaliaffou@gmail.com>
2020-02-03 13:17:55 +0530

    login verification.

--- src/app/service1/login.service.ts
+++ src/app/service1/login.service.ts
@@ -1,20 +1,51 @@
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor(private httpclient : HttpClient) { }
+  alertService: any;
+  success: boolean;
+  access_token: string;
+  constructor(private httpclient : HttpClient, private router: Router) { }
 
   // getLoginDetails(name , pass) : Promise<any>{
   //   return this.httpclient.post("http://juegostudio.in:3021/user/login", {name , pass})
   //   .toPromise();
   // }
-
-  getLoginDetails(name, pass) {
-    return this.httpclient.post("http://juegostudio.in:3021/user/login", {"email":name , "password":pass});
+  getAccessToken(){
+    return this.access_token;
   }
+
+  verifyLogin(name: string, pass: string) {
+   this.httpclient.post("http://juegostudio.in:3021/user/login", {"email":name , "password":pass}).pipe(
+      
+      catchError((error: HttpErrorResponse) =>{
+        return this.handleError(error);
+      })
+      )
+    
+    .subscribe((data: any) => {if(data.code == 200){
+      this.success = true;
+      this.access_token = data.data.userToken;
+    }},
+      (error: any) => console.log('http error',error)
+    
+    );;
+    return this.success;
+  }
+  handleError(error: HttpErrorResponse){
+    if(error.status == 401){
+      this.success=false;
+    }else{
+      return Observable.throw(error);
+    }
+  }
+
+  
 }
